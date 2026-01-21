71d7835268daaed8d5dafc03a21201d616f06cd8
jamesbatayola <batayolajakejames@gmail.com>
2026-01-21 11:09:42 +0800

    ADD: coockie parser & ejs views

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,27 @@
 import express from "express";
 
-import routes from "./routes/mainRoutes";
+import routes from "./routes/mainRoutes.js";
+
+// dirname & filename
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // NPM Packages
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// dirname
+
+app.set("view engine", "ejs");
+app.set("view", path.join(__dirname, "views"));
 
 // ----- ROUTES ----- //
 app.use("/", routes);
--- src/routes/mainRoutes.ts
+++ src/routes/mainRoutes.ts
@@ -6,4 +6,8 @@ router.get("/test", (_req, res, _next) => {
 	res.json({ data: "TEST" });
 });
 
+router.post("/shorten", (req, res, next) => {
+	const url: string = req.url;
+});
+
 export default router;
