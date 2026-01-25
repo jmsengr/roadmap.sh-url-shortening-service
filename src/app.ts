1502be1d410cb7229a87ee1b7b31ae5a3f485b82
jmsengr <batayolajakejames@gmail.com>
2026-01-25 12:43:02 +0800

    ADD: css design

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
